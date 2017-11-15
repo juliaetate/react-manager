72e1172339ee066db5eb36efd8332117caf8f787
Julia Tate <julia.e.tate@gmail.com>
2017-11-15 11:43:20 -0600

    Install Redux Thunk to build an asyn action for logging in a user and tie that action to the login form.

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
 import firebase from 'firebase';
 import reducers from './src/reducers';
 import { Header } from './src/components/common';
@@ -22,8 +23,10 @@ export default class App extends Component {
   }
 
   render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
     return (
-      <Provider store={createStore(reducers)}>
+      <Provider store={store}>
         <View>
           <Header headerText="Manager" />
           <LoginForm />
